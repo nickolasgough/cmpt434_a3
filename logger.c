3716777cb3bdcdb8d871d9fe90acf555e87d551b
Nickolas Gough <nickolas.v.gough@gmail.com>
2019-03-07 12:30:26 -0600

    Try current iteration.

--- logger.c
+++ logger.c
@@ -181,7 +181,6 @@ int main(int argc, char* argv[]) {
         if (strcmp(message, "next") != 0) {
             /* Receive each packet */
             numP = atoi(message);
-            printf("%d\n", numP);
             for (n = 0; n < numP; n += 1) {
                 memset(message, 0, MSG_SIZE);
                 recv(procFd, message, MSG_SIZE, 0);
